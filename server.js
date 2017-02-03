2e4c4e9ce9b7fe91e75ec04cad158fa54ad38004
Mitchell Sheaffer <mitchellsheaffer@gmail.com>
2017-02-03 20:31:47 +0000

    beginning upgrade process to new fb graph api tools

--- server.js
+++ server.js
@@ -2,85 +2,44 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var OAuth2 = require('oauth').Oauth2;
+var FB = require('fb');
 var app = express();
 
-var oauth2 = new OAuth2('942022655927691',
-                        '54b78203147966085cbc3814a7caeab3',
-                        '',
-                        'https://www.facebook.com/dialog/oauth',
-                        'https://graph.facebook.com/oauth/access_token',
-                        null);
+//var oauth2 = new OAuth2('942022655927691', '', '', 'https://www.facebook.com/dialog/oauth', 'https://graph.facebook.com/oauth/access_token', null);
 
 app.get('/', function(req, res) {
   
   console.log('login');
-
-  global.fbAsyncInit = function () {
-    FB.init({
-      appID   : '942022655927691',
-      status  : true,
-      xfbml   : true,
-      version : 'v2.8'
-    });
-
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    
-  };
-
-
-  
-
- /*
-  url = 'http://www.imdb.com/title/tt1229340/';  
-
-  request(url, function(error, response, html) {
-    if (!error) {
-      var $ = cheerio.load(html);
-
-      var title, release, rating;
-      var json = { title : "", release : "", rating : "" };
-
-      $('.title_wrapper').filter(function () {
-        var data = $(this);        
-        title = data.children().first().text().trim();
-        json.title = title;
-      });
-      
-      $('#titleYear').filter(function () {
-        var data = $(this);
-        release = data.children().first().text();
-        json.release = release;
-      });
-      
-      $('.title_wrapper .subtext').filter(function () {
-        var data = $(this);
-        rating = data.children().first().attr('content');
-        json.rating = rating;
-      });
-
-      console.log(json);
-      
-    }
-  });
-  */
 });
 
 app.get('/facebook/auth', (req, res) => {
-  var redirect_uri = "http://dev.mitchellsheaffer.com:3000/facebook/callback;
+  var redirect_uri = "http://dev.mitchellsheaffer.com:3000/facebook/callback";
   var params = {
     'redirect_uri' : redirect_uri,
     'scope' : 'user_about_me,user_posts'
   };
   res.redirect(oauth2.getAuthoriseUrl(params));
+});
+
+app.get('/facebook/callback', (req, res) => {
+  if (req.error_reason) {
+    console.log(req.error_reason);
+    res.send(req.error_reason); 
+  }
+  if (req.query.code) {
+    var loginCode = req.query.code;
+    var redirect_uri = "/facebook/callback";
+    oauth2.getOauthAccessToken(loginCode, {grant_type : 'authorization_code', redirect_uri : redirect_uri}, (err, accessToekn, refreshToken, params) => {
+      if (err) {
+        console.log(err);  
+        res.send(err);
+      }
+      var access_token = accessToken;
+      var expires = params.expires;
+      req.session.access_token = access_token;
+      req.session.expires = expires;
+    });
+  }
 });
 
 
